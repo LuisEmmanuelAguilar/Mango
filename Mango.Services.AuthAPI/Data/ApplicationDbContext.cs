4c5620be23b910d674770d1a1ffe7dde36db6cc4
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2024-08-14 20:10:45 -0600

    AuthAPI

---
+++ Mango.Services.AuthAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Mango.Services.AuthAPI.Models;
+
+namespace Mango.Services.AuthAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
