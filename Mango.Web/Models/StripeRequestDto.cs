d57450efaf6ae26436ea5a1e45fc93c5555f4b67
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2024-10-09 17:07:30 -0600

    Confirmation UI

---
+++ Mango.Web/Models/StripeRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Mango.Web.Models
+{
+    public class StripeRequestDto
+    {
+        public string StripeSessionUrl { get; set; }
+        public string StripeSssionId { get; set; }
+        public string ApprovedUrl { get; set; }
+        public string CancelUrl { get; set; }
+        public OrderHeaderDto OrderHeader { get; set; }
+    }
+}
