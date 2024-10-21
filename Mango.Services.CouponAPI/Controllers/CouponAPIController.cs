540176327dc11684062747f1abc323a1e47e24f0
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2024-10-21 17:30:08 -0600

    Stripe for Coupons and Checkout

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -6,6 +6,7 @@ using Mango.Services.CouponAPI.Models.Response;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.Xml;
 
 namespace Mango.Services.CouponAPI.Controllers
 {
@@ -96,6 +97,17 @@ namespace Mango.Services.CouponAPI.Controllers
                 _db.Coupons.Add(obj);
                 _db.SaveChanges();
 
+                var options = new Stripe.CouponCreateOptions
+                {
+                    AmountOff = (long)(couponDto.DiscountAmount * 100),
+                    Name = couponDto.CouponCode,
+                    Currency = "usd",
+                    Id = couponDto.CouponCode
+                };
+
+                var service = new Stripe.CouponService();
+                service.Create(options);
+
                 _response.Result = _mapper.Map<CouponDto>(obj);
             }
             catch (Exception ex)
@@ -138,6 +150,9 @@ namespace Mango.Services.CouponAPI.Controllers
                 Coupon obj = _db.Coupons.First(u => u.CouponId == id);
                 _db.Coupons.Remove(obj);
                 _db.SaveChanges();
+
+                var service = new Stripe.CouponService();
+                service.Delete(obj.CouponCode);
             }
             catch (Exception ex)
             {
