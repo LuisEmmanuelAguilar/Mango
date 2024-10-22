6648a3c0e83d5c09ae4acd92d09e8d7893157fe8
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2024-10-22 14:42:19 -0600

    Data Table JS in get all Orders

---
+++ Mango.Web/wwwroot/js/order.js
@@ -0,0 +1,19 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: "/order/getall" },
+        "columns": [
+            { data: 'orderheaderid', "width": "5%"},
+            { data: 'email', "width": "25%" },
+            { data: 'name', "width": "20%" },
+            { data: 'phone', "width": "10%" },
+            { data: 'status', "width": "10%" },
+            { data: 'orderTotal', "width": "10%" }
+        ]
+    })
+}
