e1a54f9719732be6a3d6f12c8ca4f2583637de40
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2024-10-28 20:57:56 -0600

    Updates for Order in WebProject

--- Mango.Web/wwwroot/js/order.js
+++ Mango.Web/wwwroot/js/order.js
@@ -1,14 +1,31 @@
 ﻿var dataTable;
 
 $(document).ready(function () {
-    loadDataTable();
+    var url = window.location.search;
+    if (url.includes("approved")) {
+        loadDataTable("approved");
+    }
+    else {
+        if (url.includes("readyforpickup")) {
+            loadDataTable("readyforpickup");
+        }
+        else {
+            if (url.includes("cancelled")) {
+                loadDataTable("cancelled");
+            }
+            else {
+                loadDataTable("all");
+            }
+        }
+    }
 });
 
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url: "/order/getall" },
+        order: [[0, 'desc']],
+        "ajax": { url: "/order/getall?status=" + status },
         "columns": [
-            { data: 'orderheaderid', "width": "5%"},
+            { data: 'orderHeaderId', "width": "5%" },
             { data: 'email', "width": "25%" },
             { data: 'name', "width": "20%" },
             { data: 'phone', "width": "10%" },
